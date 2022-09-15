8b3d91dd72fdd399f54b7246b0f0eaee2933afa6
Sunny Kahlon <sunny17@gmail.com>
2022-09-14 23:45:38 -0700

    Update APupload.ps1

--- APupload.ps1
+++ APupload.ps1
@@ -4,8 +4,8 @@
 #Set level temporarily to trusted
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 # Install Nuget and  Get-WindowsAutopilotinfo
-Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201 -Scope CurrentUser -Verbose -ErrorAction Stop
-Install-Script Get-WindowsAutoPilotInfo -Force -Confirm:$false -Verbose -Scope CurrentUser -ErrorAction Stop
+Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201 -Verbose
+Install-Script Get-WindowsAutoPilotInfo -Force -Confirm:$false -Verbose
 if ($?){
     Write-Host "Get-WindowsAutoPilotInfo Installed. Wait for MS login window..." -ForegroundColor Green
 }
